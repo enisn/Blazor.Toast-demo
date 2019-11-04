a500d69bd69d843674d01a20bcec27e9cf2232ac
enisn <iletisim@enisnecipoglu.com>
2019-11-04 21:12:31 +0300

    Initialization of site files

---
+++ _content/Blazor.Toast.Toastr/toastr-interop.js
@@ -0,0 +1,13 @@
+﻿window.ShowToastrFromCs = (toastType, positionClass, message) => {
+    toastr.options = {
+        "debug": false,
+        "positionClass": positionClass,
+        "onclick": null,
+        "fadeIn": 300,
+        "fadeOut": 1000,
+        "timeOut": 5000,
+        "extendedTimeOut": 1000
+    };
+
+    toastr[toastType](message);
+};
